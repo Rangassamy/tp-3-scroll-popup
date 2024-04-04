3a851c3a96c7657842fdba13b90c8cde3d47ac0c
Rangassamy <enzorangassamy@gmail.com>
2024-04-04 10:57:55 -0400

    tp finish without correction

--- script.js
+++ script.js
@@ -7,3 +7,31 @@
 // Faire apparaitre la popup quand on est en bas du site
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
+
+//-------------------------------------------------------------------//
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    navbar.style.height = "60px";
+  } else {
+    navbar.style.height = "90px";
+  }
+
+  if (window.scrollY > 250) {
+    imgImprovise.style.transform = "translateX(0)";
+    imgImprovise.style.opacity = 1;
+  }
+
+  if (window.scrollY > 1620) {
+    popup.style.transform = "translateX(0)";
+    popup.style.opacity = 1;
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.style.transform = "translateX(-200px)";
+  popup.style.opacity = 0;
+  setTimeout(() => {
+    popup.style.display = "none";
+  }, 1000);
+});
